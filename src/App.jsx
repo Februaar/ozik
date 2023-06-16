ae827012df9e73aaae5a523ad5b8408031f41411
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-16 22:49:43 +0900

    Create App.jsx
    프로젝트 폴더 수정 후 다시 업로드

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+
+import "./App.css";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <section className="drawer-content">
+        {/* <Nav /> */}
+        <section className="main">
+          <h2>Ozik</h2>
+          <ProductList products={products} addToCart={addToCart} />
+          <Cart cart={cart} />
+          <Routes>
+            {/* <Route path="*" element={<ProductList />} /> */}
+            {/* <Route path="*" element={<Product />} /> */}
+            {/* <Route path="*" element={<Cart />} /> */}
+          </Routes>
+        </section>
+        {/* <Footer /> */}
+      </section>
+    </BrowserRouter>
+  );
+};
+
+export default App;
