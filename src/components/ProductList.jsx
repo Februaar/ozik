adad9d07b2cb4e57c7d493d4267b293bc4b9807a
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-16 22:50:21 +0900

    Create ProductList.jsx

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const ProductList = ({ addToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("src/data/products.json");
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  if (products.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>상품 목록</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}원
+            <button onClick={() => addToCart(product)}>장바구니에 담기</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ProductList.propTypes = {
+  addToCart: PropTypes.func.isRequired,
+};
+
+export default ProductList;
