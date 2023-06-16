8847de2fb8fc457f0fc8d1a9bc2a2c87b11cce91
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-17 02:58:47 +0900

    Update tailwind.config.js
    fontSize 속성

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/components/Header.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: "12px",
+        base: "14px",
+        lg: "16px",
+      },
+      colors: {
+        mainBlue: "#646cff",
+      },
+    },
   },
   plugins: [],
 };
