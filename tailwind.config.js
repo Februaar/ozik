28f598880266db4cd36be8b08b89149ea227f079
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-21 04:33:11 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/components/Header.jsx"],
+  content: ["./src/components/Header.jsx", "./src/components/Footer.jsx"],
   theme: {
     extend: {
       fontSize: {
         sm: "12px",
         base: "14px",
         lg: "16px",
+        xl: "20px",
       },
       colors: {
         mainBlue: "#646cff",
