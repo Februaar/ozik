306e3e025dfe9ab2318794557c8e576fe632b849
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-22 03:54:12 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,50 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
 import Footer from "./components/Footer";
-// import ProductList from "./components/ProductList";
-// import Cart from "./components/Cart";
 
 import "./App.css";
 
 const App = () => {
-  // const [products, setProducts] = useState([]);
-  // const [cart, setCart] = useState([]);
-
-  // const addToCart = (product) => {
-  //   setCart([...cart, product]);
-  // };
-
   return (
-    <BrowserRouter>
+    <Router>
       <Header />
-      <section className="drawer-content">
-        {/* <Nav /> */}
-        <section className="main">
-          {/* <ProductList products={products} addToCart={addToCart} />
-          <Cart cart={cart} />
-          <Routes> */}
-          {/* <Route path="*" element={<ProductList />} /> */}
-          {/* <Route path="*" element={<Product />} /> */}
-          {/* <Route path="*" element={<Cart />} /> */}
-          {/* </Routes> */}
-        </section>
-        <Footer />
-      </section>
-    </BrowserRouter>
+      <Routes>
+        {/* <Route path="*" element={<Error />} />
+        <Route path="/" element={<Index />} /> */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        {/* <Route path="/product/:id" element={<Products />} />
+        <Route path="/cart" element={<Cart />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
 export default App;
+
+// return (
+//   <Router>
+//     <HeaderView />
+//     <Routes>
+//       <Route path="/login" element={<LoginPage />} />
+//       <Route path="/signup" element={<SignupPage />} />
+//     </Routes>
+//     <section className="drawer-content">
+//       {/* <Nav /> */}
+//       <section className="main">
+//         {/* <ProductList products={products} addToCart={addToCart} />
+//     <Cart cart={cart} />
+//     <Routes> */}
+//         {/* <Route path="*" element={<ProductList />} /> */}
+//         {/* <Route path="*" element={<Product />} /> */}
+//         {/* <Route path="*" element={<Cart />} /> */}
+//         {/* </Routes> */}
+//       </section>
+//       <Footer />
+//     </section>
+//   </Router>
+// );
