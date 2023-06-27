6f41d804c37524b7ae13e17dd13144df3ff7bb99
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-27 22:08:08 +0900

    Create Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Slider from "./Slider";
+
+const Main = () => {
+  return (
+    <div className="min-w-screen-lg w-[400px] pt-[60px] pb-[160px] mx-auto">
+      <Slider />
+    </div>
+  );
+};
+
+export default Main;
