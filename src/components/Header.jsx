79a094cbdb1295704503ef8fb3ca09dbfb19bbc5
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-29 22:47:40 +0900

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,145 +2,135 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 
+import CategoryList from "../components/CategoryList";
+import Slider from "../components/Slider";
+
 const Header = () => {
   const { user, logout } = useContext(AuthContext);
 
   const handleLogout = () => {
     logout();
+    window.location.reload();
   };
-  return (
-    // Header 전체
-    <header>
-      {/* Header 영역 */}
-      <div className="relative w-[1050px] h-[100px] mx-auto">
-        {/* 회원가입 영역 */}
-        <div className="absolute top-2 right-0 z-20 flex items-center text-sm">
-          {user ? (
-            <>
-              <button onClick={handleLogout}>로그아웃</button>
-              <div className="w-[0.5px] h-[15px] mr-2 ml-2 bg-gray-400"></div>
-              <Link to="/customer-service">고객센터</Link>
-            </>
-          ) : (
-            <>
-              <Link to="/login">로그인</Link>
-              <div className="w-[0.5px] h-[15px] mr-2 ml-2 bg-gray-400"></div>
-              <Link to="/customer-service">고객센터</Link>
-            </>
-          )}
-        </div>
 
-        {/* 검색 바 영역 */}
-        <div className="absolute h-[100px] pt-[36px] items-center mx-auto">
-          {/* 로고 홈 버튼 */}
-          <div className="h-[63px] flex items-center">
-            <img src="/images/logo.png" alt="오직로고" className="w-8 h-9" />
-            <button className="font-semibold text-blue-600 ml-5 w-25 h-15 text-xl">
+  return (
+    <header>
+      <div data-v-1d0b532e data-v-33d79d7a className="header fixed">
+        {/* header_top */}
+        <div data-v-33d79d7a className="header_top">
+          <div data-v-33d79d7a className="top_inner flex">
+            {user ? (
+              <ul data-v-33d79d7a className="top_list flex">
+                <li data-v-33d79d7a className="top_item">
+                  <Link
+                    data-v-33d79d7a
+                    to="/customer-service"
+                    className="top_link"
+                  >
+                    고객센터
+                  </Link>
+                </li>
+                <li data-v-33d79d7a className="top_item">
+                  <Link data-v-33d79d7a to="/my-page" className="top_link">
+                    마이페이지
+                  </Link>
+                </li>
+                <li data-v-33d79d7a className="top_item">
+                  <Link data-v-33d79d7a to="/subscription" className="top_link">
+                    구독상품
+                  </Link>
+                </li>
+                <li data-v-33d79d7a className="top_item">
+                  <button
+                    data-v-33d79d7a
+                    onClick={handleLogout}
+                    className="top_link"
+                  >
+                    로그아웃
+                  </button>
+                </li>
+              </ul>
+            ) : (
+              <ul data-v-33d79d7a className="top_list flex">
+                <li data-v-33d79d7a className="top_item">
+                  <Link
+                    data-v-33d79d7a
+                    to="/customer-service"
+                    className="top_link"
+                  >
+                    고객센터
+                  </Link>
+                </li>
+                <li data-v-33d79d7a className="top_item">
+                  <Link to="/my-page" className="top_link">
+                    마이페이지
+                  </Link>
+                </li>
+                <li data-v-33d79d7a className="top_item">
+                  <Link data-v-33d79d7a to="/subscription" className="top_link">
+                    구독상품
+                  </Link>
+                </li>
+                <li data-v-33d79d7a className="top_item">
+                  <Link data-v-33d79d7a to="/login" className="top_link">
+                    로그인
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
+        {/* header_main */}
+        <div className="data-v-33d79d7a header_main">
+          <div className="data-v-33d79d7a main_inner flex">
+            <h1>
+              <Link to="/" aria-label="로고" className="data-v-33d79d7a logo">
+                OZIK
+              </Link>
+            </h1>
+            <div className="right">
+              <div data-v-6e10f65a className="gnb_area flex">
+                <nav data-v-6e10f65a className="gnb">
+                  <ul data-v-6e10f65a className="gnb_list flex">
+                    <li data-v-6e10f65a data-v-24a540ed className="gnb_item">
+                      <Link data-v-24a540ed to="/" className="gnb_link active">
+                        HOME
+                      </Link>
+                    </li>
+                    <li data-v-6e10f65a data-v-24a540ed className="gnb_item">
+                      <Link data-v-24a540ed to="/" className="gnb_link">
+                        TYPE
+                      </Link>
+                    </li>
+                    <li data-v-6e10f65a data-v-24a540ed className="gnb_item">
+                      <Link data-v-24a540ed to="/" className="gnb_link">
+                        SHOP
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+                <div className="search_btn_box flex">
+                  <Link to="#" aria-label="검색">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="black"
+                      className="bi bi-search"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            {/* <button className="font-semibold text-blue-600 ml-5 w-25 h-15 text-xl">
               OZIK
-            </button>
-          </div>
-          {/* 검색 바 */}
-          <div className="relative w-[1050px] mx-auto">
-            <div className="flex items-center justify-between absolute left-[325px] top-[-55px] w-[400px] h-[45px] pl-14 border border-blue-600 rounded-md bg-white shadow-md">
-              <input
-                id="gnb-search"
-                placeholder="오직 나를 위해 구독해보세요"
-                required
-                className="w-300 bg-transparent border-none outline-none text-base tracking-tighter"
-              />
-              <button
-                id="submit"
-                aria-label="submit"
-                className="relative w-[30px] h-[30px] m-[10px] text-base flex items-center justify-center"
-              >
-                <img
-                  src="./images/search.svg"
-                  alt="search-img"
-                  className="w-[full] h-[full]"
-                />
-              </button>
-            </div>
-          </div>
-          {/* 장바구니 연결 */}
-          <div className="relative w-[1050px] mx-auto">
-            <div className="absolute flex items-center justify-center top-[-49px] right-[0px]">
-              <button
-                className="w-[36px] h-[36px] mr-[15px]"
-                aria-label="구매내역"
-              >
-                <img
-                  src="./images/calendar.svg"
-                  alt="box-img"
-                  className="w-[22px] h-[22px] mx-auto"
-                />
-              </button>
-              <button
-                className="w-[36px] h-[36px] mx-[5px]"
-                aria-label="구독하기"
-              >
-                <img
-                  src="./images/subscribe.svg"
-                  alt="box-img"
-                  className="w-[22px] h-[22px] mx-auto"
-                />
-              </button>
-              <button
-                className="w-[36px] h-[36px] ml-[15px]"
-                aria-label="마이페이지"
-              >
-                <img
-                  src="./images/person.svg"
-                  alt="box-img"
-                  className="w-[22px] h-[22px] mx-auto"
-                />
-              </button>
-            </div>
+            </button> */}
           </div>
         </div>
-      </div>
-      {/* 카테코리 영역 */}
-      <div className="relative min-w-[1050px] w-full z-10 tracking-tight bg-white">
-        <div className="relative flex justify-between w-[1050px] h-[56px] mx-auto">
-          {/* 카테고리 Nav */}
-          <div>
-            <div className="flex items-center h-full">
-              <span className="w-[22px] h-[22px] mr-[14px]">
-                <img
-                  src="/images/category.svg"
-                  alt="category.img"
-                  className="w-full mx-auto"
-                />
-              </span>
-              <span className="font-medium tracking-tighter text-gray-700 text-lg">
-                카테고리
-              </span>
-            </div>
-          </div>
-          {/* 키워드별 추천 Nav */}
-          <ul className="flex ml-[-100px]">
-            <li className="flex justify-center w-[150px] h-[55px] pt-[18px] leading-5 text-center">
-              <span className="h-fit-content text-lg font-medium text-gray-700 cursor-pointer">
-                신상품
-              </span>
-            </li>
-            <li className="flex justify-center w-[150px] h-[55px] pt-[18px] leading-5 text-center">
-              <span className="h-fit-content text-lg font-medium text-gray-700 cursor-pointer">
-                베스트
-              </span>
-            </li>
-            <li className="flex justify-center w-[150px] h-[55px] pt-[18px] leading-5 text-center">
-              <span className="h-fit-content text-lg font-medium text-gray-700 cursor-pointer">
-                알뜰쇼핑
-              </span>
-            </li>
-            <li className="flex justify-center w-[150px] h-[55px] pt-[18px] leading-5 text-center">
-              <span className="h-fit-content text-lg font-medium text-gray-700 cursor-pointer">
-                특가/혜택
-              </span>
-            </li>
-          </ul>
-          <div></div>
-        </div>
+        <CategoryList />
       </div>
       {/* <Slider /> */}
     </header>
