230be3764761fb4c28cfe5c2cf247f15f6ae4787
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-29 22:47:26 +0900

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -20,3 +20,32 @@ Cart.propTypes = {
 };
 
 export default Cart;
+
+{
+  /* 장바구니 연결 */
+}
+<div className="relative w-[1050px] mx-auto">
+  <div className="absolute flex items-center justify-center top-[-49px] right-[0px]">
+    <button className="w-[36px] h-[36px] mr-[15px]" aria-label="구매내역">
+      <img
+        src="./images/calendar.svg"
+        alt="box-img"
+        className="w-[22px] h-[22px] mx-auto"
+      />
+    </button>
+    <button className="w-[36px] h-[36px] mx-[5px]" aria-label="구독하기">
+      <img
+        src="./images/subscribe.svg"
+        alt="box-img"
+        className="w-[22px] h-[22px] mx-auto"
+      />
+    </button>
+    <button className="w-[36px] h-[36px] ml-[15px]" aria-label="마이페이지">
+      <img
+        src="./images/person.svg"
+        alt="box-img"
+        className="w-[22px] h-[22px] mx-auto"
+      />
+    </button>
+  </div>
+</div>;
