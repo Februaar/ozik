8a9dde2c5116428c52e4758689853d07cef02e0e
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-30 22:06:18 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ export default {
   content: [
     "./src/App.jsx",
     "./src/components/Header.jsx",
-    "./src/components/Footer.jsx",
-    "./src/components/Index.jsx",
     "./src/components/Login.jsx",
-    "./src/components/Signup.jsx",
     "./src/components/Email.jsx",
     "./src/components/Password.jsx",
     "./src/components/SocialLogin.jsx",
+    "./src/components/Signup.jsx",
     "./src/components/CategoryList.jsx",
+    "./src/components/Index.jsx",
+    "./src/components/Contents.jsx",
     "./src/components/Slider.jsx",
+    "./src/components/Footer.jsx",
   ],
   theme: {
     extend: {
