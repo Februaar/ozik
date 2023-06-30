b6edda7e7f76802b6987d4ead4474da0a3598612
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-01 04:16:44 +0900

    Update App.jsx
    손대지마!!

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import MyPage from "./components/MyPage";
 import Footer from "./components/Footer";
-import ScrollButton from "./view/ScrollButton";
-import { AuthProvider } from "./contexts/AuthContext";
+import ProductDetails from "./components/ProductDetails";
 
 import Recommendations from "./categories/Recommendations";
 import NewProducts from "./categories/NewProducts";
 import BestProducts from "./categories/BestProducts";
 import SpecialOffers from "./categories/SpecialOffers";
+
+import ScrollButton from "./view/ScrollButton";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const App = () => {
   return (
@@ -26,15 +28,16 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/my-page" element={<MyPage />} />
-
           {/* <Route path="/cart" element={<Cart />} /> */}
 
           {/* 카테고리 별 경로 */}
-          {/* <Route path="/product/:id" element={<Products />} /> */}
-          <Route path="/recommendation" element={<Recommendations />} />
+          <Route path="/recommendation " element={<Recommendations />} />
           <Route path="/new" element={<NewProducts />} />
           <Route path="/best" element={<BestProducts />} />
           <Route path="/special" element={<SpecialOffers />} />
+
+          {/* 동적 경로 */}
+          <Route path="/:category/:productId" element={<ProductDetails />} />
 
           {/* 에러 페이지 */}
           {/* <Route path="*" element={<Error />} /> */}
@@ -48,25 +51,22 @@ const App = () => {
 
 export default App;
 
-// return (
-//   <Router>
-//     <HeaderView />
-//     <Routes>
-//       <Route path="/login" element={<LoginPage />} />
-//       <Route path="/signup" element={<SignupPage />} />
-//     </Routes>
-//     <section className="drawer-content">
-//       {/* <Nav /> */}
-//       <section className="main">
-//         {/* <ProductList products={products} addToCart={addToCart} />
-//     <Cart cart={cart} />
-//     <Routes> */}
-//         {/* <Route path="*" element={<ProductList />} /> */}
-//         {/* <Route path="*" element={<Product />} /> */}
-//         {/* <Route path="*" element={<Cart />} /> */}
-//         {/* </Routes> */}
-//       </section>
-//       <Footer />
-//     </section>
-//   </Router>
-// );
+// useEffect(() => {
+//   const dataPath = `src/data/recommendation.json`;
+
+//   fetch(dataPath)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const selectedProduct = data.find(
+//         (item) => item.id === parseInt(productId)
+//       );
+//       setProduct(selectedProduct);
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching product details:", error);
+//     });
+// }, [category, productId]);
+
+// if (!product) {
+//   return <div>Loading...</div>;
+// }
