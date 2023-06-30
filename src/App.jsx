0323160ecfdcaa93d2028dd8e8b378e0bbcb4893
Kyungjin Jung <kyoooungjin@gmail.com>
2023-06-30 22:07:09 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Index from "./components/Index";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import MyPage from "./components/MyPage";
+import Footer from "./components/Footer";
+import ScrollButton from "./view/ScrollButton";
 import { AuthProvider } from "./contexts/AuthContext";
+
+import Recommendations from "./categories/Recommendations";
+import NewProducts from "./categories/NewProducts";
+import BestProducts from "./categories/BestProducts";
+import SpecialOffers from "./categories/SpecialOffers";
 
 const App = () => {
   return (
@@ -13,14 +20,27 @@ const App = () => {
       <Router>
         <Header />
         <Routes>
-          {/* <Route path="*" element={<Error />} /> */}
+          {/* 기본 경로 */}
           <Route path="/" element={<Index />} />
+
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          {/* <Route path="/product/:id" element={<Products />} /> */}
+          <Route path="/my-page" element={<MyPage />} />
+
           {/* <Route path="/cart" element={<Cart />} /> */}
+
+          {/* 카테고리 별 경로 */}
+          {/* <Route path="/product/:id" element={<Products />} /> */}
+          <Route path="/recommendation" element={<Recommendations />} />
+          <Route path="/new" element={<NewProducts />} />
+          <Route path="/best" element={<BestProducts />} />
+          <Route path="/special" element={<SpecialOffers />} />
+
+          {/* 에러 페이지 */}
+          {/* <Route path="*" element={<Error />} /> */}
         </Routes>
         <Footer />
+        <ScrollButton />
       </Router>
     </AuthProvider>
   );
