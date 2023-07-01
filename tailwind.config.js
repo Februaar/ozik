8f7514737f92a49e1d72f88983a02eaf4c9624a8
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-02 08:06:11 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,26 @@ export default {
     "./src/components/Password.jsx",
     "./src/components/SocialLogin.jsx",
     "./src/components/Signup.jsx",
-    "./src/components/CategoryList.jsx",
+
     "./src/components/Index.jsx",
-    "./src/components/Contents.jsx",
+    "./src/components/Category.jsx",
+    "./src/components/ProductList.jsx",
+    "./src/components/ProductDetails.jsx",
     "./src/components/Slider.jsx",
     "./src/components/Footer.jsx",
+    "./src/components/Search.jsx",
+
+    "./src/categories/BestProducts.jsx",
+    "./src/categories/NewProducts.jsx",
+    "./src/categories/Recommendations.jsx",
+    "./src/categories/SpecialOffers.jsx",
+
+    "./src/view/ScrollButton.jsx",
+    "./src/view/Error.jsx",
+
+    // 미완성
+    "./src/components/MyPage.jsx",
+    "./src/components/Customer.jsx",
   ],
   theme: {
     extend: {
