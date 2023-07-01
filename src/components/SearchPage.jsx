b2fa9eb415b9d6bdb5c675d8f4ffd462cb691172
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-02 08:05:26 +0900

    Create SearchPage.jsx

---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,14 @@
+import Search from "./Search";
+
+// 검색페이지 레이아웃
+const SearchPage = () => {
+  return (
+    <div className="layer lg layer_search lg" data-v-78455e2e data-v-c13ac206>
+      <div className="layer_container" data-v-78455e2e>
+        <Search />
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
