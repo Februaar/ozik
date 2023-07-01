7bc422ec551a148543605280cfc527453e52bf12
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-02 08:05:31 +0900

    Create SubScription.jsx

---
+++ src/components/SubScription.jsx
@@ -0,0 +1,10 @@
+const SubScription = () => {
+  return (
+    <div className="min-h-screen flex flex-row items-center justify-center">
+      <h2 className="text-[23px] font-bold">구독상품 페이지</h2>
+      <p className="mt-4">를 준비중입니다.</p>
+    </div>
+  );
+};
+
+export default SubScription;
