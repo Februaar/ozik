1376723b52fb87c2ad1a58c341c491f59b998d7c
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-02 08:04:23 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ import Index from "./components/Index";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import MyPage from "./components/MyPage";
+import Customer from "./components/Customer";
+import SubScription from "./components/SubScription";
+
 import Footer from "./components/Footer";
 import ProductDetails from "./components/ProductDetails";
+import SearchPage from "./components/SearchPage";
 
 import Recommendations from "./categories/Recommendations";
 import NewProducts from "./categories/NewProducts";
@@ -14,6 +18,7 @@ import BestProducts from "./categories/BestProducts";
 import SpecialOffers from "./categories/SpecialOffers";
 
 import ScrollButton from "./view/ScrollButton";
+import Error from "./view/Error";
 import { AuthProvider } from "./contexts/AuthContext";
 
 const App = () => {
@@ -24,23 +29,26 @@ const App = () => {
         <Routes>
           {/* 기본 경로 */}
           <Route path="/" element={<Index />} />
-
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/my-page" element={<MyPage />} />
-          {/* <Route path="/cart" element={<Cart />} /> */}
+          <Route path="/customer-service" element={<Customer />} />
+          <Route path="/subscription" element={<SubScription />} />
 
           {/* 카테고리 별 경로 */}
           <Route path="/recommendation " element={<Recommendations />} />
-          <Route path="/new" element={<NewProducts />} />
+          <Route path="/newData" element={<NewProducts />} />
           <Route path="/best" element={<BestProducts />} />
           <Route path="/special" element={<SpecialOffers />} />
 
           {/* 동적 경로 */}
           <Route path="/:category/:productId" element={<ProductDetails />} />
 
+          {/* Search 페이지 */}
+          <Route path="/search" element={<SearchPage />} />
+
           {/* 에러 페이지 */}
-          {/* <Route path="*" element={<Error />} /> */}
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
         <ScrollButton />
@@ -50,23 +58,3 @@ const App = () => {
 };
 
 export default App;
-
-// useEffect(() => {
-//   const dataPath = `src/data/recommendation.json`;
-
-//   fetch(dataPath)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const selectedProduct = data.find(
-//         (item) => item.id === parseInt(productId)
-//       );
-//       setProduct(selectedProduct);
-//     })
-//     .catch((error) => {
-//       console.error("Error fetching product details:", error);
-//     });
-// }, [category, productId]);
-
-// if (!product) {
-//   return <div>Loading...</div>;
-// }
