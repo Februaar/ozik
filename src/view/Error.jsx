2b8ebba541c0d26b76d47da4bd96ff44f6a4b56c
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-02 08:06:06 +0900

    Create Error.jsx

---
+++ src/view/Error.jsx
@@ -0,0 +1,11 @@
+const Error = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[340px] h-14 rounded-xl bg-black bg-opacity-40 text-white text-center flex items-center justify-center z-10">
+        <p>세상에서 가장 지루한 중학교는 로딩 중입니다.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
