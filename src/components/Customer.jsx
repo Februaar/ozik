13ab5d9b2e23fa53b1804f19c4cedc7b4a80cad9
Kyungjin Jung <kyoooungjin@gmail.com>
2023-07-02 08:05:08 +0900

    Create Customer.jsx

---
+++ src/components/Customer.jsx
@@ -0,0 +1,10 @@
+const Customer = () => {
+  return (
+    <div className="min-h-screen flex flex-row items-center justify-center">
+      <h2 className="text-[23px] font-bold">고객센터 페이지</h2>
+      <p className="mt-4">를 준비중입니다.</p>
+    </div>
+  );
+};
+
+export default Customer;
